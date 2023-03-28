a18d6b756aa7050802e3ed8c7dad38bfce5e014b
TeakIvy <legocjones@gmail.com>
2023-03-27 22:01:36 -0400

    Synchronization

--- src/main/java/me/teakivy/teakstweaksloader/TeaksTweaksLoader.java
+++ src/main/java/me/teakivy/teakstweaksloader/TeaksTweaksLoader.java
@@ -2,6 +2,7 @@ package me.teakivy.teakstweaksloader;
 
 import me.teakivy.teakstweaksloader.util.FileDownloader;
 import me.teakivy.teakstweaksloader.util.Logger;
+import me.teakivy.teakstweaksloader.util.PluginUnloader;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.InvalidDescriptionException;
 import org.bukkit.plugin.InvalidPluginException;
@@ -13,40 +14,46 @@ import java.io.IOException;
 
 public final class TeaksTweaksLoader extends JavaPlugin {
 
+    private final String dlUrl = "https://api.spiget.org/v2/resources/94021/download";
+
     @Override
-    public void onEnable() {
+    public synchronized void onEnable() {
         Plugin plugin = Bukkit.getPluginManager().getPlugin("TeaksTweaks");
 
         if (plugin != null) {
             Logger.log(Logger.LogLevel.INFO, "Disabling Teak's Tweaks...");
-            Bukkit.getPluginManager().disablePlugin(plugin);
+            PluginUnloader.unload(plugin);
         }
 
-        Bukkit.getScheduler().runTaskLater(this, () -> {
-            File file = new File("plugins/TeaksTweaks.jar");
-            if (file.exists()) {
-                Logger.log(Logger.LogLevel.INFO, "Deleting TeaksTweaks.jar...");
-                file.delete();
-            }
+        File file = new File("plugins/TeaksTweaks.jar");
+        if (file.exists()) {
+            Logger.log(Logger.LogLevel.INFO, "Deleting TeaksTweaks.jar...");
+            file.delete();
+        }
 
-            try {
-                Logger.log(Logger.LogLevel.INFO, "Downloading TeaksTweaks.jar...");
-                FileDownloader.downloadFile("https://api.spiget.org/v2/resources/94021/download", "plugins/TeaksTweaks.jar");
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+        try {
+            Logger.log(Logger.LogLevel.INFO, "Downloading TeaksTweaks.jar...");
+            FileDownloader.downloadFile(dlUrl, "plugins/TeaksTweaks.jar");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-            try {
-                Logger.log(Logger.LogLevel.INFO, "Loading TeaksTweaks.jar...");
-                Bukkit.getPluginManager().loadPlugin(new File("plugins/TeaksTweaks.jar"));
-            } catch (InvalidPluginException | InvalidDescriptionException e) {
-                e.printStackTrace();
-            }
+        try {
+            Logger.log(Logger.LogLevel.INFO, "Loading Teak's Tweaks.jar...");
+            Bukkit.getPluginManager().loadPlugin(new File("plugins/TeaksTweaks.jar"));
+        } catch (InvalidPluginException | InvalidDescriptionException e) {
+            e.printStackTrace();
+        }
 
-            Plugin teaksTweaks = Bukkit.getPluginManager().getPlugin("TeaksTweaks");
-            Logger.log(Logger.LogLevel.INFO, "Enabling TeaksTweaks.jar...");
-            Bukkit.getPluginManager().enablePlugin(teaksTweaks);
-        }, 20L * 2);
+        Plugin teaksTweaks = Bukkit.getPluginManager().getPlugin("TeaksTweaks");
+        Logger.log(Logger.LogLevel.INFO, "Enabling Teak's Tweaks.jar...");
+
+        if (teaksTweaks == null) {
+            Logger.log(Logger.LogLevel.ERROR, "Teak's Tweaks.jar failed to load!");
+            return;
+        }
+
+        Bukkit.getPluginManager().enablePlugin(teaksTweaks);
     }
 
     @Override
